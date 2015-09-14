bf7eba9c6949fca63a44d4c638fcc8f2b5d53814
alvarojoao <alvaro@silvino.me>
2015-09-14 19:15:59 -0300

    setup the special products

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -80,6 +80,31 @@ angular.module('core').controller('HomeController', ['$scope','$timeout', 'Authe
 
 	      		});
 		};
+		$scope.initDestaque = function(id){
+			$('#'+id).owlCarousel({
+
+				loop:true,
+			    margin:10,
+			    responsiveClass:true,
+			    responsive:{
+			        0:{
+			            items:1,
+			            nav:true
+			        },
+			        600:{
+			            items:3,
+			            nav:false
+			        },
+			        1000:{
+			            items:5,
+			            nav:true,
+			            loop:false
+			        }
+			    }
+
+      		});
+		};
+		
 	 	
 
 		$scope.stopPromotionBannerBlocker = function(){
