defc1eedd0560536b64044b015d563b97ad1ac7f
alvarojoao <alvaro@silvino.me>
2015-09-26 16:07:10 -0300

    fixing small issues for like and share for facebook

---
+++ app/models/reprint.server.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+var DBRef = mongoose.SchemaTypes.DBRef;
+
+/**
+ * Product Schema
+ */
+var objectReprintSchema = {
+	
+	createdDate: {
+		type: Date,
+		default: Date.now
+	},
+	name: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	email: {
+		type: String,
+		default:''
+	},
+	productTitle: {
+		type: String,
+		default:''
+	},
+	productSlug: {
+		type: String,
+		default:''
+	}
+};
+var ReprintSchema = new Schema(objectReprintSchema);
+exports.getReprintSchema = function(){
+	return objectReprintSchema;
+};
+mongoose.model('Reprint', ReprintSchema,'reprint');
